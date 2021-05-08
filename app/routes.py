2d755e1b8ad45ab810709de7c859de04d9aa2775
torrua <torrua@gmail.com>
2021-05-08 19:15:08 +0700

    Update partial search and keys highlighting

--- app/routes.py
+++ app/routes.py
@@ -95,7 +95,6 @@ def submit_search():
     word = request.form.get("word", "")
     current_event_id = request.form.get("current_event_id", Event.latest().id)
     is_case_sensitive = bool(distutils.util.strtobool(request.form.get("case_sensitive", False)))
-    is_partial_results = bool(distutils.util.strtobool(request.form.get("partial_results", False)))
 
     if not word:
         return jsonify(result="<div></div>")
@@ -110,8 +109,7 @@ def submit_search():
         result = Word.html_all_by_name(
             name=word, style=DEFAULT_HTML_STYLE,
             event_id=current_event_id,
-            case_sensitive=is_case_sensitive,
-            partial_results=is_partial_results)
+            case_sensitive=is_case_sensitive)
 
         if not result:
             result = nothing % f"There is no word <b>{word}</b> in Loglan. Try switching to English" \
@@ -120,8 +118,8 @@ def submit_search():
     elif search_language == "eng":
         result = Word.translation_by_key(
             key=word, style=DEFAULT_HTML_STYLE,
-            case_sensitive=is_case_sensitive,
-            partial_results=is_partial_results)
+            event_id=current_event_id,
+            case_sensitive=is_case_sensitive)
 
         if not result:
             result = nothing % f"There is no word <b>{word}</b> in English. Try switching to Loglan" \
